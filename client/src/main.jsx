aabc884e648d718431606e7db832c7f468cf14eb
Mohak325 <mohakharsh5678@gmail.com>
2025-09-26 17:10:09 +0530

    fixed mc

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
 import App from './App.jsx';
 import './index.css';
-import Navbar from './navbar.jsx';
-import { motion } from "framer-motion";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-      <Navbar />
-    
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
